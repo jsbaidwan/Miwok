2159cb3aa54134662b614e50172457fd821458ee
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-06-18 17:02:58 +0530

    add numbers to NumberActivity using String array

--- app/src/main/java/com/example/jaspreetsingh/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/miwok/NumbersActivity.java
@@ -9,5 +9,18 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Create a array of words
+        String[] words = new String[10];
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[7] = "seven";
+        words[8] = "eight";
+        words[9] = "nine";
+        words[10] = "ten";
     }
 }
