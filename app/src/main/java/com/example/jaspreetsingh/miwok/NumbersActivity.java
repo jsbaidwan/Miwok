566d41edf9557c440810be7c4faec2a96ef0f928
Jaspreet Singh (DELL) <cgc.jsbaidwan@gmail.com>
2017-06-19 17:03:15 +0530

    display numbers the logs verbose of NumbersActivity

--- app/src/main/java/com/example/jaspreetsingh/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/jaspreetsingh/miwok/NumbersActivity.java
@@ -2,6 +2,9 @@ package com.example.jaspreetsingh.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import static android.util.Log.v;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -18,9 +21,21 @@ public class NumbersActivity extends AppCompatActivity {
         words[3] = "four";
         words[4] = "five";
         words[5] = "six";
-        words[7] = "seven";
-        words[8] = "eight";
-        words[9] = "nine";
-        words[10] = "ten";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumbersActivity", "Word at Index 0: " + words[0]);
+        Log.v("NumbersActivity", "Word at Index 1: " + words[1]);
+        Log.v("NumbersActivity", "Word at Index 2: " + words[2]);
+        Log.v("NumbersActivity", "Word at Index 3: " + words[3]);
+        Log.v("NumbersActivity", "Word at Index 4: " + words[4]);
+        Log.v("NumbersActivity", "Word at Index 5: " + words[5]);
+        Log.v("NumbersActivity", "Word at Index 6: " + words[6]);
+        Log.v("NumbersActivity", "Word at Index 7: " + words[7]);
+        Log.v("NumbersActivity", "Word at Index 8: " + words[8]);
+        Log.v("NumbersActivity", "Word at Index 9: " + words[9]);
+
     }
 }
